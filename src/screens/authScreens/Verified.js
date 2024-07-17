a47fd410cc04ab510eaf63a246c217be329f1b8b
ViiesTech <visstechapps@gmail.com>
2024-07-17 19:06:25 +0500

    code

--- src/screens/authScreens/Verified.js
+++ src/screens/authScreens/Verified.js
@@ -7,27 +7,42 @@ import {Button} from '../../componets/Button';
 const Verified = ({navigation}) => {
   return (
     <ImageBackground style={{flex: 1}} source={Images.background}>
-                      <ImageBackground style={{flex:1}} source={Images.layer}>
+      <ImageBackground style={{flex: 1}} source={Images.layer}>
+        <View style={{flex: 1}}>
+          <View style={{alignItems: 'center', padding: 30}}>
+            <Image source={Images.logo2} />
+          </View>
 
-      <View style={{flex: 1}}>
-        <View style={{alignItems: 'center', padding: 30}}>
-          <Image source={Images.logo2} />
-        </View>
-      
           <View
             style={{
               backgroundColor: '#00190A',
               width: '100%',
               flex: 1,
               padding: 40,
-              justifyContent:'center',
-              alignItems:'center',
+              justifyContent: 'center',
+              alignItems: 'center',
               borderTopEndRadius: 70,
               borderTopLeftRadius: 70,
             }}>
-              <Image source={Images.verified}/>
-              <Text style={{color:'white',alignSelf:'center',textAlign:'center',marginTop:20,marginBottom:30,fontWeight:'bold',fontSize:18}}>Successfully{'\n'}Verified</Text>
-            <Button navigation={navigation} navigateTo={'login'} color={'#29CF6E'} title={'Back To Login'} />
+            <Image source={Images.verified} />
+            <Text
+              style={{
+                color: 'white',
+                alignSelf: 'center',
+                textAlign: 'center',
+                marginTop: 20,
+                marginBottom: 30,
+                fontWeight: 'bold',
+                fontSize: 18,
+              }}>
+              Successfully{'\n'}Verified
+            </Text>
+            <Button
+              navigation={navigation}
+              navigateTo={'login'}
+              color={'#29CF6E'}
+              title={'Back To Login'}
+            />
             <View></View>
           </View>
         </View>
